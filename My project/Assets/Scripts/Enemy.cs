fd8e7600b718b913899c3014030519f4ce534c2f
QKeeper <moncer256710@gmail.com>
2025-05-03 18:25:29 +0500

    fix: ui sprites

--- My project/Assets/Scripts/Enemy.cs
+++ My project/Assets/Scripts/Enemy.cs
@@ -1,7 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
+
+[Serializable]
+public class Speed
+{
+    public LevelIdentifier levelId;
+    [Range(0.5f, 3)]public float value;
+}
 
 public class Enemy : MonoBehaviour
 {
@@ -10,6 +19,9 @@ public class Enemy : MonoBehaviour
     public static UnityEvent<Enemy> OnDeath = new();
     
     private float speed = 1f;
+
+    public List<Speed> speeds = new();
+
     private Transform target;
 
     void Start()
@@ -17,6 +29,12 @@ public class Enemy : MonoBehaviour
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         totalAlive += 1;
         OnSpawn.Invoke(this);
+
+        var data = speeds.FirstOrDefault((item) => item.levelId == LevelManager.Instance.levelId);
+        Debug.Log(data);
+
+        Debug.Log(data.value);
+        speed = data.value;
     }
 
     private void OnDestroy()
@@ -28,21 +46,10 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         if (target.position.x > transform.position.x)
-        {
             transform.eulerAngles = new Vector3(0, 180, 0);
-        }
         else
-        {
             transform.eulerAngles = new Vector3(0, 0, 0);
-        }
+
         transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-        if (PauseMenu.GameIsPause) 
-        {
-            speed = 0;
-        }
-        else
-        {
-            speed = 2f;
-        }
     }
 }
